3d3f924b83869fdef6557c9daaafea908a0fb46c
Md. Jahid Hasan <jahid.csedu@gmail.com>
2024-05-12 13:37:47 +0900

    Added customer logger

--- src/main/java/org/example/OrderBook.java
+++ src/main/java/org/example/OrderBook.java
@@ -5,11 +5,12 @@ import org.example.repository.OrderBookRepository;
 import org.example.service.OrderBookManagerService;
 import org.example.util.PrinterService;
 import org.example.util.VolumeChangeCalculator;
+import org.example.util.logger.CustomLogger;
 
 import java.util.HashMap;
 
 public class OrderBook {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args){
         PrinterService printerService = new PrinterService();
         OrderBookRepository orderBookRepository = new OrderBookRepository(new HashMap<>());
         VolumeChangeCalculator volumeChangeCalculator = new VolumeChangeCalculator(orderBookRepository, printerService);
@@ -17,8 +18,12 @@ public class OrderBook {
         new OrderBookManagerService(printerService, orderBookRepository);
 
         while (true) {
-            Thread.sleep(ConfigurationManager.getVolumeChangeCalculationInterval() * 1000);
-            volumeChangeCalculator.calculateVolumeChangeForAllSymbols();
+            try {
+                Thread.sleep(ConfigurationManager.getVolumeChangeCalculationInterval() * 1000);
+                volumeChangeCalculator.calculateVolumeChangeForAllSymbols();
+            } catch (InterruptedException e) {
+                CustomLogger.error("Failed: "+e.getMessage(), e);
+            }
         }
     }
 }
